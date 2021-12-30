769ee8472f2cd518d0ff58b7e2b4968849b85428
Serenacula <faylee@hanginggardens.com>
2021-12-30 17:31:22 +0000

    adding public folder for github pages.

---
+++ public/component---src-pages-blog-js-e46e01a8fc0fc13c8395.js
@@ -0,0 +1,2 @@
+(self.webpackChunkfaylees_portfolio=self.webpackChunkfaylees_portfolio||[]).push([[7],{4216:function(e,n,t){"use strict";var u=t(7294);n.Z=function(e){var n=e.children,t=u.useRef(null);return u.useEffect((function(){}),[]),u.createElement("main",{ref:t},n)}},223:function(e,n,t){"use strict";t.r(n);var u=t(7294),l=t(4216);n.default=function(){return u.createElement(l.Z,null,u.createElement("p",null,"boooo"))}}}]);
+//# sourceMappingURL=component---src-pages-blog-js-e46e01a8fc0fc13c8395.js.map
